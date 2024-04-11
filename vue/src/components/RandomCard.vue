ab856c728dcf1b3d541d153236d7ec09565af555
Cheryl Bradshaw <cdbradshaw@gmail.com>
2024-04-11 10:38:20 -0400

    merge fix

--- vue/src/components/RandomCard.vue
+++ vue/src/components/RandomCard.vue
@@ -1,19 +1,12 @@
 <template>
     <div class="randomCard">
         <router-link v-bind:to="{name: 'RandomCard' }">
-<<<<<<< HEAD
-          <h1>Random Card:</h1>
-          <h1>{{ card.name }}</h1>
-          <!-- <img v-bind:src="card.image_uris.small"/> -->
-        
 
-        </router-link>
-=======
         <img v-bind:src="card.image_uris.small"/>
         <h1>Random Card Goes Here</h1>
         <h1>{{ card.name }}</h1>
     </router-link>
->>>>>>> b653d6c0da73e5383efb0eb89c100c79f7aa49fc
+
     </div>
 </template>
 
@@ -35,11 +28,7 @@ export default {
     };
   },
     created() {
-<<<<<<< HEAD
       
-=======
-    
->>>>>>> b653d6c0da73e5383efb0eb89c100c79f7aa49fc
     CardService.random()
       .then(response => {
         this.card = response.data;
