e5011923d5d17edb524b240ec8fa4247aa24acfa
Cheryl Bradshaw <cdbradshaw@gmail.com>
2024-04-12 10:10:40 -0400

    CB merge

--- vue/src/components/ViewCollections.vue
+++ vue/src/components/ViewCollections.vue
@@ -1,15 +1,10 @@
 <template>
-<<<<<<< HEAD
-    <div class="container">
-        <h1>View Collections</h1>
-=======
     <router-link v-bind:to="{ name: 'view-collections' }"></router-link>
 
 
     <div class="publicCollections">
         <h1>View Public Collections</h1>
         <!-- <magic-card v-for="card in collection" v-bind:="card" v-bind:key="card_id" /> -->
->>>>>>> 3f8fa1394b0cd6a3ba02f4ee298445bdae6eef12
         <p>Collection 1</p>
         <p>Collection 2</p>
         <p>Collection 3</p>
