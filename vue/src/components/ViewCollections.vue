5de1ea3aae2bcb48b1c0b80ca8816956efce677b
Israel Ryan <izzyryan603@gmail.com>
2024-04-15 14:55:15 -0400

    IR: username fix -backend

--- vue/src/components/ViewCollections.vue
+++ vue/src/components/ViewCollections.vue
@@ -1,77 +1,72 @@
 <template>
   <div id="collectionsContainer">
     <div class="publicCollections" v-bind:to="{ name: 'view-collections' }">
-      <!-- <div class="moreCollections" v-bind:to="{ name: 'userName' }"> -->
       <h1 id="publicTitle">Viewable Card Collections</h1>
       <div id="collectionResults" v-for="collection in collections" v-bind:key="collection.collectionId">
-        <h2 v-show="!showCards">View the {{ collection.collectionName }} collection from userId {{ user.username }}</h2>
+        <h2 v-show="!showCards"> View the {{ collection.collectionName }} collection from userId {{ collection.username }}</h2>
       </div>
 
       <div id="cards" v-show="showCards">
-        <!-- <card-front /> -->
-
-      </div>
-    <!-- </div> -->
+        </div>
     </div>
-
-
 
     <div class="userCollections" v-bind:to="{ name: 'userName' }">
       <h1>{{ this.$store.state.user.username }} Collections</h1>
       <p>My Collection 1</p>
       <p>My Collection 2</p>
     </div>
-
-
-
   </div>
 </template>
 
-<script>
-import CollectionService from '../services/CollectionService';
-import CardFront from '../components/CardFront.vue';
-import AuthService from '../services/AuthService';
+  <script>
+  import CollectionService from '../services/CollectionService';
+  import CardFront from '../components/CardFront.vue';
+  import AuthService from '../services/AuthService';
 
-export default {
-  components: {
-    CardFront
-  },
+  export default {
+    components: {
+      CardFront
+    },
 
-  data() {
-    return {
-      // userId: collection.userId,
-      user: {},
-      showCards: false,
-      collections:
-        [{}]
+    data() {
+      return {
+        // userId: collection.userId,
+        user: {},
+        showCards: false,
+        collections:
+          [{}]
+      }
+    },
+    created() {
+
+      CollectionService.getAllCollections()
+        .then(response => {
+          this.collections = response.data;
+
+        })
+    },
+    methods: {
+      showCollectionCards(value) {
+        this.showCards = value;
+      },
+      getUserById(userId) {
+      AuthService.getUserbyId(userId) // Assuming AuthService provides getUserbyId
+        .then(response => {
+          this.user = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching user:', error);
+        });
     }
-  },
-  created() {
-
-    CollectionService.getAllCollections()
-      .then(response => {
-        this.collections = response.data;
-
-      }),
-      AuthService.getUserbyId(this.userId)
-      .then(response => {
-        this.user = response.data;
-
-      });
-  },
-  methods: {
-    showCollectionCards(value) {
-      this.showCards = value;
     }
+
   }
 
-}
+  </script>
 
-</script>
-
-<style scoped>
-.collectionsContainer {
-  border-style: dashed;
-  border-color: rgb(252, 34, 26);
-}
-</style>
+  <style scoped>
+  .collectionsContainer {
+    border-style: dashed;
+    border-color: rgb(252, 34, 26);
+  }
+  </style>
