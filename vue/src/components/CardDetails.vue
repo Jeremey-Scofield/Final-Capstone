00f0ff2f6622c6125d011e2660c217589553bbbb
Jonathan Sinclair <jonathansinclair83@gmail.com>
2024-04-15 11:53:44 -0400

    JS made changes to card details

--- vue/src/components/CardDetails.vue
+++ vue/src/components/CardDetails.vue
@@ -1,63 +1,125 @@
 <template>
-    
-    <div id="detailsHeader">
-         <h2>Card Details:</h2>
-    </div>
-
-    <table>
-        <tr>
-            <th>Card Name:</th>
-            <td>{{ card.name }}</td>
-            <th>Rarity:</th>
-            <td>{{ card.rarity }}</td>
-        </tr>
-        <tr>
-            <th>Mana Cost:</th>
-            <td>{{ card.mana_cost }}</td>
-            <th>Set:</th>
-            <td>{{ card.set }}</td>
-        </tr>
-        <tr>
-            <th>Colors:</th>
-            <td>{{ card.colors }}</td>
-            <th>Set Name:</th>
-            <td>{{ card.set_name }}</td>
-        </tr>
-        <tr>
-            <th>ColorIdentity:</th>
-            <td>{{ card.color_identity }}</td>
-            <th>Text:</th>
-            <td>{{ card.oracle_text }}</td>
-        </tr>
-        <tr>
-            <th>Type:</th>
-            <td>{{ card.type_line }}</td>
-        
-            <th>Flavor Text:</th>
-            <td>{{ card.flavor_text }}</td>
-        </tr>
-        <tr>
-            <th>Artist:</th>
-            <td>{{ card.artist }}</td>
-        </tr>
-    </table>
+  <div id="detailsHeader">
+    <h2>Card Details:</h2>
+  </div>
+  <div id="card-details">
+    <div id="card-name">Card Name:</div>
+    <div id="card-text">{{ card.name }}</div>
+    <div id="artist">Artist:</div>
+    <div id="artist-text">{{ card.artist }}</div>
+    <div id="mana-cost">Mana Cost:</div>
+    <div id="mana-text">{{ card.mana_cost }}</div>
+    <div id="rarity">Rarity:</div>
+    <div id="rarity-text">{{ card.rarity }}</div>
+    <div id="colors">Colors:</div>
+    <div id="colors-text">{{ card.colors }}</div>
+    <div id="set">Set Name:</div>
+    <div id="set-text">{{ card.set_name }}</div>
+    <div id="color-identity">ColorIdentity:</div>
+    <div id="color-identity-text">{{ card.color_identity }}</div>
+    <div id="type">Type:</div>
+    <div id="type-text">{{ card.type_line }}</div>
+    <div id="flavor">Flavor Text:</div>
+    <div id="flavor-text">{{ card.flavor_text }}</div>
+    <div id="text">Text:</div>
+    <div id="text-text">{{ card.oracle_text }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['card']
-  
-  
-
-}
-
+  props: ["card"],
+};
 </script>
 
 <style>
-
-table, th, td {
-  border: 1px solid;
+#card-name {
+  font-weight: bold;
+  grid-area: card-name;
+}
+#card-text {
+  grid-area: card-text;
+}
+#artist {
+  font-weight: bold;
+  grid-area: artist;
+}
+#artist-text {
+  grid-area: artist-text;
+}
+#mana-cost {
+  font-weight: bold;
+  grid-area: mana-cost;
+}
+#mana-text {
+  grid-area: mana-text;
+}
+#rarity {
+  font-weight: bold;
+  grid-area: rarity;
+}
+#rarity-text {
+  grid-area: rarity-text;
+}
+#colors {
+  font-weight: bold;
+  grid-area: colors;
+}
+#colors-text {
+  grid-area: colors-text;
+}
+#set {
+  font-weight: bold;
+  grid-area: set;
+}
+#set-text {
+  grid-area: set-text;
+}
+#color-identity {
+  font-weight: bold;
+  grid-area: color-identity;
+}
+#color-identity-text {
+  grid-area: color-identity-text;
+}
+#type {
+  font-weight: bold;
+  grid-area: type;
+}
+#type-text {
+  grid-area: type-text;
 }
 
+#flavor {
+  font-weight: bold;
+  grid-area: flavor;
+}
+#flavor-text {
+  grid-area: flavor-text;
+}
 
+#text {
+  font-weight: bold;
+  grid-area: text;
+}
+
+#text-text {
+  grid-area: text-text;
+}
+
+#card-details {
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-areas:
+    "card-name card-text"
+    "artist artist-text"
+    "mana-cost mana-text"
+    "rarity rarity-text"
+    "colors colors-text"
+    "set set-text"
+    "color-identity color-identity-text"
+    "type type-text"
+    "flavor flavor-text"
+    "text text-text";
+}
 </style>
