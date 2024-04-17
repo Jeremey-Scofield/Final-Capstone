227822be3a12664742457c251ad6d8a442050f1e
Israel Ryan <izzyryan603@gmail.com>
2024-04-17 14:11:57 -0400

    IR: add to collection fix

--- vue/src/components/CardFront.vue
+++ vue/src/components/CardFront.vue
@@ -1,36 +1,68 @@
 <template>
     <div class="cardFront" v-on:click="showCardDetails(true)">
-        <h4 class="card-name">{{ cardFront.name }}</h4>
-        <img class="card-image" v-bind:src="cardFront.image_uris.small" v-show="!showDetails" />
-
+      <h4 class="card-name">{{ cardFront.name }}</h4>
+      <img class="card-image" v-bind:src="cardFront.image_uris.small" v-show="!showDetails" />
     </div>
     <div id="cardDetails" v-on:click="showCardDetails(false)" v-show="showDetails">
-        <card-details v-bind:card="this.cardFront" />
+      <card-details v-bind:card="this.cardFront" />
     </div>
-</template>
   
-<script>
-import CardDetails from '../components/CardDetails.vue';
+    <div>
+      <button v-on:click="pushCardToCollection">Add To Collection</button>
+      <select v-model="selectedOption" :items="options">
+  <option v-for="option in options" :key="option.id" :value="option.collectionId">
+    {{ option.collectionName }} (ID: {{ option.collectionId }})
+  </option>
+</select>
 
-export default {
+    </div>
+  
+    <div> {{ cardFront.id }}</div>
+
+
+  </template>
+  
+  <script>
+  import CardDetails from '../components/CardDetails.vue';
+  import axios from 'axios';
+  
+  export default {
     components: {
-        CardDetails
-    },
-    props: ['cardFront'],
-    methods: {
-        showCardDetails(value) {
-            this.showDetails = value;
-        }
+      CardDetails
     },
     data() {
         return {
-
-
-            showDetails: false
+            showDetails: false,
+            options: [],
+            collectionId: '',
         }
+    },
+    created() {
+      this.getCollectionsByUserId();
+    },
+    props: ['cardFront'],
+    methods: {
+      showCardDetails(value) {
+        this.showDetails = value;
+      },
+      getCollectionsByUserId() {
+        axios.get(`http://localhost:9000/collections/user/${this.$store.state.user.id}`)
+          .then(response => {
+            this.options = response.data;
+          })
+          .catch(error => {
+            console.error("Error fetching collections:", error.message);
+          });
+      },
+      pushCardToCollection() {
+        axios.post(`http://localhost:9000/collections/cards/new`, {
+          collectionId: this.selectedOption, // Access collection ID from selected option
+          cardId: 'hello'
+        })
+      }
     }
-}
-</script>
+  }
+  </script>
 
   
 <style scoped>
