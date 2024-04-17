486d32937797277fd4f0ae820839307414524199
Jeremey Scofield <jeremey.scofiel@gmail.com>
2024-04-17 16:05:50 -0400

    JS Merge

--- vue/src/components/CardFront.vue
+++ vue/src/components/CardFront.vue
@@ -7,16 +7,6 @@
         <card-details v-bind:card="this.cardFront" />
     </div>
 
-<<<<<<< HEAD
-  <div id="toggleButton">
-    <button v-on:click="toggleDropdown">Add To Collection</button>
-    <select v-model="selectedOption" :items="options" v-if="showCardDetails" v-on:input="addToCollection">
-      <option v-for="option in options" :key="option">
-        {{ option }}
-      </option>
-    </select>
-  </div>
-=======
     <div>
         <button v-on:click="pushCardToCollection">Add To Collection</button>
         <select v-model="selectedOption" :items="options">
@@ -27,22 +17,17 @@
     </div>
 
     
->>>>>>> 4720d9642ba75f6bb1c6178ac4c6f9a6038559f6
 </template>
 
 <script>
 import axios from 'axios';
 import CardDetails from '../components/CardDetails.vue';
-import axios from 'axios';
+
 
 export default {
     components: {
         CardDetails
     },
-<<<<<<< HEAD
-    created() {
-    this.getCollectionsByUserId();
-=======
     data() {
         return {
             showDetails: false,
@@ -52,30 +37,12 @@ export default {
     },
     created() {
         this.getCollectionsByUserId();
->>>>>>> 4720d9642ba75f6bb1c6178ac4c6f9a6038559f6
     },
     props: ['cardFront'],
     methods: {
         showCardDetails(value) {
             this.showDetails = value;
         },
-<<<<<<< HEAD
-        getCollectionsByUserId(userId) {
-            axios.get(`http://localhost/9000/collections/user/${this.$store.state.user.id}`)
-                .then(response => {
-                    this.options = response.data;
-            })
-        },
-        addToCollection() {
-      this.addToCollection.push(this.selectedOption);
-
-    },
-    },
-    data() {
-        return {
-            showDetails: false,
-            options: []
-=======
         getCollectionsByUserId() {
             axios.get(`http://localhost:9000/collections/user/${this.$store.state.user.id}`)
                 .then(response => {
@@ -90,7 +57,6 @@ export default {
                 collectionId: this.selectedOption, // Access collection ID from selected option
                 cardId: this.cardFront.id
             })
->>>>>>> 4720d9642ba75f6bb1c6178ac4c6f9a6038559f6
         }
     }
 }
