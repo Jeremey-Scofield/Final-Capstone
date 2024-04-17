26c23ec9aa6a22e01202b1bc00c81b6ad670b36a
Cheryl Bradshaw <cdbradshaw@gmail.com>
2024-04-17 11:33:00 -0400

    CB styling

--- vue/src/components/PageHeader.vue
+++ vue/src/components/PageHeader.vue
@@ -1,14 +1,21 @@
 <template>
-    <div class="nav">
-         <h1> Magic: The Gathering Slytherin Card Collector</h1>
-      <div id="login">
+  <div class="headContainer">
+    <div id="title">
+      <h1> Magic: The Gathering Slytherin Card Collector</h1>
+    </div>
+
+    <div id="login">
+      <div id="links">
         <router-link v-bind:to="{ name: 'home' }">Home | </router-link>
-        
+
         <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-        <router-view/>
-        
+      </div>
+
+      <div id="register">
+        <router-view />
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -16,10 +23,37 @@
 </script>
 
 <style scoped>
-.nav {
-  display: flex;
+.headContainer {
+  display: grid;
+  
+  grid-auto-columns: 1fr;
+  grid-template-areas: 
+    "title"
+    "login";
 }
-h1 {
-  padding-right: 25px;
+h1{
+  font-size: 2.5em;
+}
+#title {
+  grid-area: title;
+  place-self: center;
+  color: rgb(54, 104, 175);
+}
+
+#links {
+  grid-area: links;
+
+}
+
+#register {
+  grid-area: register;
+  
+}
+#login {
+  display: grid;
+  grid-area: login;
+  grid-auto-columns: 1fr 4fr;
+  grid-template-areas: 
+    "links register";
 }
 </style>
