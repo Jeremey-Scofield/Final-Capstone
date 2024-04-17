e390bc82d3c07e7b2ae96a958b581fc329d79050
Jonathan Sinclair <jonathansinclair83@gmail.com>
2024-04-17 13:30:59 -0400

    JS fixing merge issue

--- vue/src/components/CardFront.vue
+++ vue/src/components/CardFront.vue
@@ -7,26 +7,48 @@
     <div id="cardDetails" v-on:click="showCardDetails(false)" v-show="showDetails">
         <card-details v-bind:card="this.cardFront" />
     </div>
+
+  <div id="toggleButton">
+    <button v-on:click="toggleDropdown">Add To Collection</button>
+    <select v-model="selectedOption" :items="options" v-if="showCardDetails" v-on:input="addToCollection">
+      <option v-for="option in options" :key="option">
+        {{ option }}
+      </option>
+    </select>
+  </div>
 </template>
   
 <script>
+import axios from 'axios';
 import CardDetails from '../components/CardDetails.vue';
 
 export default {
     components: {
         CardDetails
     },
+    created() {
+    this.getCollectionsByUserId();
+    },
     props: ['cardFront'],
     methods: {
         showCardDetails(value) {
             this.showDetails = value;
-        }
+        },
+        getCollectionsByUserId(userId) {
+            axios.get(`http://localhost/9000/collections/user/${this.$store.state.user.id}`)
+                .then(response => {
+                    this.options = response.data;
+            })
+        },
+        addToCollection() {
+      this.addToCollection.push(this.selectedOption);
+
+    },
     },
     data() {
         return {
-
-
-            showDetails: false
+            showDetails: false,
+            options: []
         }
     }
 }
