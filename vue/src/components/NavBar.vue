334541575214eeb510a5feb6b194560cf6afc22f
Israel Ryan <izzyryan603@gmail.com>
2024-04-17 22:13:32 -0400

    IR: additional features, reactive methods

--- vue/src/components/NavBar.vue
+++ vue/src/components/NavBar.vue
@@ -1,102 +1,110 @@
- <template>
-  
-  
+<template>
+
+
   <div class="navLinks" v-if="$store.state.token != ''">
-  
-  <button id="myCollections">View My Collections</button> 
-  <br />
-  <button id="createCollection" v-on:click="showCollectionForm"> Create Collection </button>
+
+    <button id="myCollections">View My Collections</button>
+    <br />
+    <button id="createCollection" v-on:click="showCollectionForm"> Create Collection </button>
 
 
-  <div v-if="showCollectForm" id="createCollection">
-    <div id="create-collection">
-      <form v-on:submit.prevent="createCollection">
-        <div>
-          Name:<input type="text" id="collection_name" v-model="collection.collection_name" placeholder="New Collection Name"/>
+    <div v-if="showCollectForm" id="createCollection">
+      <div id="create-collection">
+        <form v-on:submit.prevent="createCollection">
+          <div>
+            Name:<input type="text" id="collection_name" v-model="collection.collection_name"
+              placeholder="New Collection Name" />
 
-          <!-- ID: <input type="text" id="user_id" v-model="this.$store.state.user.id" disabled />  -->
-        </div>
+            <!-- ID: <input type="text" id="user_id" v-model="this.$store.state.user.id" disabled />  -->
+          </div>
 
-        <button type="submit">Confirm Collection</button>
-      </form>
-    </div>
-  </div>
-
-  <br />
-
-  <div>
-        <button v-on:click="deleteCollectionByCollectionId(selectedOption)">Delete Collection </button>
-        <select v-model="selectedOption" :items="options">
-            <option v-for="option in options" :key="option.id" :value="option.collectionId">
-                {{ option.collectionName }} (ID: {{ option.collectionId }})
-            </option>
-        </select>
+          <button type="submit">Confirm Collection</button>
+        </form>
+      </div>
     </div>
 
+    <br />
+
+    <div>
+      <button v-on:click="deleteCollectionByCollectionId(selectedOption)">Delete Collection </button>
+      <select v-model="selectedOption" :items="options">
+        <option v-for="option in options" :key="option.id" :value="option.collectionId">
+          {{ option.collectionName }} (ID: {{ option.collectionId }})
+        </option>
+      </select>
+    </div>
+
   </div>
-<img src="../images/mtgsymbols-removebg-preview.png" />
-  
-</template> 
+  <img src="../images/mtgsymbols-removebg-preview.png" />
+
+</template>
 
 <script>
-  import axios from 'axios';
+import axios from 'axios';
+import ViewCollections from './ViewCollections.vue';
 
-  export default{
-    methods: {
-      showCollectionForm() {
-        this.showCollectForm = !this.showCollectForm;
-      },
-      createCollection() {
-        const collectionName = String(this.collection.collection_name).trim();
-        const userId = Number(this.collection.user_id);
+export default {
+  methods: {
+    showCollectionForm() {
+      this.showCollectForm = !this.showCollectForm;
+    },
+    createCollection() {
+      const collectionName = String(this.collection.collection_name).trim();
+      const userId = Number(this.collection.user_id);
 
-        console.log("hello");
+      console.log("hello");
 
-        axios.post('http://localhost:9000/collections/new', {
+      axios.post('http://localhost:9000/collections/new', {
 
-          collectionName: collectionName,
-          userId: userId
-          })
-          
-          .then((response) => {
-          console.log("hello");
-          });
+        collectionName: collectionName,
+        userId: userId
+      })
 
-        this.collection.collection_name = "";
+        .then((response) => {
+          this.getCollectionsByUserId();
+        });
 
-        this.showCollectForm = !this.showCollectForm;
+      this.collection.collection_name = "";
 
-      },
-      getCollectionsByUserId() {
-            axios.get(`http://localhost:9000/collections/user/${this.$store.state.user.id}`)
-                .then(response => {
-                    this.options = response.data;
-                })
-                .catch(error => {
-                    console.error("Error fetching collections:", error.message);
-                });
-        },
-        deleteCollectionByCollectionId(collectionId) {
-          axios.delete(`http://localhost:9000/collections/delete/${collectionId}`);
+      this.showCollectForm = !this.showCollectForm;
 
+    },
+    getCollectionsByUserId() {
+      axios.get(`http://localhost:9000/collections/user/${this.$store.state.user.id}`)
+        .then(response => {
+          this.options = response.data;
+        })
+        .catch(error => {
+          console.error("Error fetching collections:", error.message);
+        });
+    },
+    deleteCollectionByCollectionId(collectionId) {
+      axios.delete(`http://localhost:9000/collections/delete/${collectionId}`)
+        .then(() => {
+          console.log("Collection deleted successfully");
+          // Re-fetch collections after deletion
           this.getCollectionsByUserId();
 
-        }
-    },
-    data() {
-      return {
-        showCollectForm: false,
-        collection: {
-          collection_name: "",
-          user_id: this.$store.state.user.id
-        },
-        options: [],
-      };
-    },
-    created() {
-      this.getCollectionsByUserId();
+        })
+        .catch(error => {
+          console.error("Error deleting collection:", error.message);
+        });
     }
-  };
+  },
+  data() {
+    return {
+      showCollectForm: false,
+      collection: {
+        collection_name: "",
+        user_id: this.$store.state.user.id
+      },
+      options: [],
+    };
+  },
+  created() {
+    this.getCollectionsByUserId();
+  }
+};
 
 
 </script>
@@ -108,19 +116,19 @@ button {
   height: 30px;
   border-radius: 5px;
 }
+
 img {
   width: 150px;
   margin-left: auto;
   margin-right: auto;
   display: block;
 }
+
 #myCollections {
-background-color: rgb(246, 220, 159);
+  background-color: rgb(246, 220, 159);
 }
 
-#createCollection{
+#createCollection {
   background-color: rgb(90, 169, 62);
 }
-
-
 </style>
